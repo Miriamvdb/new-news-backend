1daa1ebd9764cd6da8b13672b12b26a495c0546f
Miriamvdb <miriamvdb@live.nl>
2022-08-01 11:52:17 +0200

    Feature 4: Article details with comments

--- routers/article.js
+++ routers/article.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-const Article = require("../models").article;
 const Category = require("../models").category;
+const Article = require("../models").article;
+const Comment = require("../models").comment;
 const router = new Router();
 
 // Feature 2: http GET :4000/articles
@@ -16,4 +17,31 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// Feature 4: http GET :4000/articles/1
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const article = await Article.findByPk(id, {
+      include: [Category],
+    });
+    res.json(article);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+// Feature 4: http GET :4000/articles/1/comments
+router.get("/:id/comments", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const article = await Article.findByPk(id, {
+      include: [Comment],
+    });
+    res.json(article);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
 module.exports = router;
